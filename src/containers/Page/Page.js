5a935e579eeba520913578ae9d4f1b366af7fc8e
Alex <57138526+alexnikitin341@users.noreply.github.com>
2020-05-08 12:30:56 +0300

    add form authorization

--- src/containers/Page/Page.js
+++ src/containers/Page/Page.js
@@ -1,14 +1,52 @@
-import React, { Component } from "react";
-import classes from "./Page.module.css";
+import React, { Component } from 'react'
+import classes from './Page.module.css'
+import { Form, Field } from 'react-final-form'
+import Styles from './Styles'
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const onSubmit = async (values) => {
+  await sleep(300)
+}
 
 class Page extends Component {
   render() {
     return (
-      <div className={classes.Page}>
-        <h1>Hellow World</h1>
-      </div>
-    );
+      <Styles>
+        <div className={classes.Page}>
+          <h1>Login</h1>
+          <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label>Email</label>
+                  <Field
+                    name="email"
+                    component="input"
+                    type="email"
+                    placeholder="Enter Email"
+                  />
+                </div>
+                <div>
+                  <label>Password</label>
+                  <Field
+                    name="password"
+                    component="input"
+                    type="password"
+                    placeholder="Enter Password"
+                  />
+                </div>
+                <div className="buttons">
+                  <button type="submit">Login</button>
+                </div>
+              </form>
+            )}
+          />
+        </div>
+      </Styles>
+    )
   }
 }
 
-export default Page;
+export default Page
