b334a6c0ed165493405ac5c06f9b6df913d93af8
TLevi <levitsku76@gmail.com>
2020-05-08 15:52:38 +0300

    add style form register

--- src/containers/Authorization/Authorization.js
+++ src/containers/Authorization/Authorization.js
@@ -9,36 +9,84 @@ const onSubmit = async () => {
   await sleep(300)
 }
 
+const Error = ({ name }) => (
+  <Field
+    name={name}
+    subscription={{ touched: true, error: true }}
+    render={({ meta: { touched, error } }) =>
+      touched && error ? <span>{error}</span> : null
+    }
+  />
+)
+
 class Authorization extends Component {
   render() {
     return (
       <Styles>
-        <div >
-          <h1>Authorization</h1>
+        <div>
+          <h1>Sign up</h1>
           <Form
             onSubmit={onSubmit}
             render={({ handleSubmit }) => (
               <form onSubmit={handleSubmit}>
                 <div>
+                  <label>Sign up</label>
+                  <Field
+                    name="Sign up"
+                    component="input"
+                    type="Sign up"
+                    placeholder="Enter Sign up"
+                  />
+                </div>
+                <div>
+                  <label>User name</label>
+                  <Field
+                    name="User name"
+                    component="input"
+                    type="User name"
+                    placeholder="Enter User name"
+                  />
+                </div>
+                <div>
                   <label>Email</label>
                   <Field
-                    name="email"
+                    name="Email"
                     component="input"
-                    type="email"
+                    type="Email"
                     placeholder="Enter Email"
                   />
                 </div>
                 <div>
+                  <label>Age</label>
+                  <Field
+                    name="age"
+                    component="input"
+                    type="number"
+                    placeholder="Age"
+                  />
+                  <Error name="age" />
+                </div>
+                <div>
                   <label>Password</label>
                   <Field
-                    name="password"
+                    name="Password"
                     component="input"
-                    type="password"
+                    type="Password"
                     placeholder="Enter Password"
                   />
                 </div>
+                <div>
+                  <label>Repeat password</label>
+                  <Field
+                    name="Repeat password"
+                    component="input"
+                    type="Repeat password"
+                    placeholder="Enter Repeat password"
+                  />
+                </div>
+
                 <div className="buttons">
-                  <button type="submit">Login</button>
+                  <button type="submit">Sign up</button>
                 </div>
               </form>
             )}
