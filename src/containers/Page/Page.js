0bc923ca6c9c4c13d88fc836ef2d194cdc7bc7be
Alex <57138526+alexnikitin341@users.noreply.github.com>
2020-05-08 11:04:13 +0300

    add react-route, blueprint, eslint

---
+++ src/containers/Page/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import classes from "./Page.module.css";
+
+class Page extends Component {
+  render() {
+    return (
+      <div className={classes.Page}>
+        <h1>Hellow World</h1>
+      </div>
+    );
+  }
+}
+
+export default Page;
