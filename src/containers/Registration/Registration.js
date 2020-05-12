a28a4556a128d233e055c90ab976b643cde3cb81
TLevi <levitsku76@gmail.com>
2020-05-12 14:26:00 +0300

    add Login

--- src/containers/Registration/Registration.js
+++ src/containers/Registration/Registration.js
@@ -3,33 +3,20 @@ import classes from './Registration.module.css'
 import { Form, Field } from 'react-final-form'
 import Styles from './Styles'
 import { FormGroup, InputGroup } from '@blueprintjs/core'
+import {
+  composeValidators,
+  validateEmail,
+  required,
+  minLength,
+  haveOneUppercase,
+  haveOneNumeral,
+} from '../../services/validation'
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const onSubmit = async () => {
   await sleep(300)
 }
-
-const composeValidators = (...validators) => (value) =>
-  validators.reduce((error, validator) => error || validator(value), undefined)
-
-const validateEmail = (value) =>
-  value.match(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/g)
-    ? undefined
-    : 'enter correct Email'
-
-const required = (value) => (value ? undefined : 'Required')
-
-const minLength = (value = '') =>
-  value.split('').length < 6 ? 'Input at min 6 symbol' : undefined
-
-const haveOneUppercase = (value = '') =>
-  value.match(/[A-Z]/g) === null
-    ? 'at least one uppercase letter is required'
-    : undefined
-
-const haveOneNumeral = (value = '') =>
-  value.replace(/\D+/g, '') === '' ? 'at least one digit is needed' : undefined
 
 const Error = ({ name }) => (
   <Field
@@ -41,7 +28,7 @@ const Error = ({ name }) => (
   />
 )
 
-class Authorization extends Component {
+class Registration extends Component {
   render() {
     return (
       <div className={classes.form_Registration}>
@@ -82,4 +69,4 @@ class Authorization extends Component {
   }
 }
 
-export default Authorization
+export default Registration
