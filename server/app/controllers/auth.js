a61dbe3698df57e677daa0315ace7b1298ed6d16
Alex <alexnikitin341@gmail.com>
2020-05-18 13:43:13 +0300

    add registration

--- server/app/controllers/auth.js
+++ server/app/controllers/auth.js
@@ -1,61 +1,57 @@
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const authHelper = require("../helpers/authHelpers");
-const { secret } = require("../../config/app").jwt;
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const authHelper = require('../helpers/authHelpers')
+const { secret } = require('../../config/app').jwt
 
-const User = mongoose.model("User");
-const Token = mongoose.model("Token");
+const User = mongoose.model('User')
+const Token = mongoose.model('Token')
 
 const updateTokens = (userId) => {
-  const accessToken = authHelper.generateAccessToken(userId);
-  const refreshToken = authHelper.generateRefreshToken();
+  const accessToken = authHelper.generateAccessToken(userId)
+  const refreshToken = authHelper.generateRefreshToken()
 
   return authHelper.replaceDbRefreshToken(refreshToken.id, userId).then(() => ({
     accessToken,
     refreshToken: refreshToken.token,
-  }));
-};
+  }))
+}
 
 const signIn = (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   User.findOne({ email })
     .exec()
     .then((user) => {
       if (!user) {
-        res.status(401).json({ message: "User does not exist!" });
+        res.status(401).json({ message: 'User does not exist!' })
       }
-      const isValid = password === user.password;
+
+      const isValid = password === user.password
+
       if (isValid) {
-        updateTokens(user._id).then((tokens) => res.json(tokens));
+        updateTokens(user._id).then((tokens) => res.json(tokens))
       } else {
-        res.status(401).json({ message: "Invalid credentials" });
+        res.status(401).json({ message: 'Invalid credentials' })
       }
     })
-    .catch((err) => res.status(500).json({ message: err.message }));
-};
-
-const create = (req, res) => {
-  User.create(req.body)
-    .then((createUser) => res.json(createUser))
-    .catch((err) => res.status(500).json(err));
-};
+    .catch((err) => res.status(500).json({ message: err.message }))
+}
 
 const refreshTokens = (req, res) => {
-  const { refreshToken } = req.body;
-  let payload;
+  const { refreshToken } = req.body
+  let payload
   try {
-    payload = jwt.verify(refreshToken, secret);
-    if (payload.type !== "refresh") {
-      res.status(400).json({ message: "Invalid token!" });
-      return;
+    payload = jwt.verify(refreshToken, secret)
+    if (payload.type !== 'refresh') {
+      res.status(400).json({ message: 'Invalid token!' })
+      return
     }
   } catch (e) {
     if (e instanceof jwt.TokenExpiredError) {
-      res.status(400).json({ message: "Token expired!" });
-      return;
+      res.status(400).json({ message: 'Token expired!' })
+      return
     } else if (e instanceof jwt.TokenExpiredError) {
-      res.status(400).json({ message: "Invalid token!" });
-      return;
+      res.status(400).json({ message: 'Invalid token!' })
+      return
     }
   }
 
@@ -63,15 +59,15 @@ const refreshTokens = (req, res) => {
     .exec()
     .then((token) => {
       if (token === null) {
-        throw new Error("Invalid token!");
+        throw new Error('Invalid token!')
       }
-      return updateTokens(token.userId);
+      return updateTokens(token.userId)
     })
     .then((tokens) => res.json(tokens))
-    .catch((err) => res.status(400).json({ message: err.message }));
-};
+    .catch((err) => res.status(400).json({ message: err.message }))
+}
 
 module.exports = {
   signIn,
   refreshTokens,
-};
+}
--- server/app/controllers/registration.js
+++ server/app/controllers/registration.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose");
-
-const Product = mongoose.model("User");
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
 
 const create = (req, res) => {
-  Product.create(req.body)
-    .then((createProduct) => res.json(createProduct))
-    .catch((err) => res.status(500).json(err));
-};
+  User.create(req.body)
+    .then((createUser) => res.json(createUser))
+    .catch((err) => res.status(500).json(err))
+}
 
 module.exports = {
   create,
-};
+}
