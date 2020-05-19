536c9d3062bf7fe0e697f8b22ee06ad92d6ba526
Alex <alexnikitin341@gmail.com>
2020-05-19 12:41:13 +0300

    return User on info-user

--- server/app/controllers/info.js
+++ server/app/controllers/info.js
@@ -2,34 +2,45 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const { secret } = require('../../config/app').jwt
 
-// const Token = mongoose.model('Token')
+const User = mongoose.model('User')
 
 const infoUser = (req, res) => {
   const { token } = req.body
-  let payload = jwt.verify(token, secret)
-  
+  let payload
 
-  // try {
-  //   payload = jwt.verify(token)
-  //   res.json({ token })
+  try {
+    payload = jwt.verify(token, secret)
+    
+    if (payload.type !== 'access') {
+      
+      res.status(400).json({ message: 'Invalid tokensw!' })
+    }
 
-  //   if (payload.type !== 'refresh') {
-  //     res.status(400).json({ message: 'Invalid token!' })
-  //     return res.json({ token })
-  //   }
-  // } catch (e) {
-  //   if (e instanceof jwt.TokenExpiredError) {
-  //     res.status(400).json({ message: 'Token expired!' })
-  //     return res.json({ token })
-  //   } else if (e instanceof jwt.TokenExpiredError) {
-  //     res.status(400).json({ message: 'Invalid token!' })
-  //     return res.json({ token })
-  //   }
-  // }
-  return res.json({
-    token,
-    payload: payload,
-  })
+    User.findById(payload.userId, function (err, user) {
+      if (err) {
+        res.status(500).json({ message: 'Invalid server!' })
+        return
+      }
+      if (user === 0) {
+        res.status(404).json({ message: 'User not found!' })
+        return
+      }
+      return res.json({
+        token: token,
+        name: user.name,
+        email: user.email,
+        age: user.age
+      })
+    })
+  } catch (e) {
+    if (e instanceof jwt.TokenExpiredError) {
+      res.status(400).json({ message: 'Token expired!' })
+      return
+    } else if (e instanceof jwt.TokenExpiredError) {
+      res.status(400).json({ message: 'Invalid token!' })
+      return
+    }
+  }
 
   // Token.findOne({ tokenId: payload.id })
   //   .exec()
