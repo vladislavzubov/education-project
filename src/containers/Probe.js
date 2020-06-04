8ef8dcc3590b209444970081e33c39bf0367a5bf
Alex <alexnikitin341@gmail.com>
2020-06-04 15:05:08 +0300

    [front] probe

--- src/containers/Probe.js
+++ src/containers/Probe.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from '../component/Menu/Menu';
 import helperMenu from '../helper/helperMenu';
 import Testing from '../component/Testing/Testing';
+import MonacoEditor from 'react-monaco-editor';
 
 const tests = [
   {
@@ -46,7 +47,7 @@ const tests = [
     time: 30,
     question: 'назовите хук который отвечает за сохранение стэйта',
     lectory: 'asdfasdf455d',
-    correctAnswer: ["setState"],
+    correctAnswer: ['setState'],
     answers: ['setState', 'useState', 'className', 'dispatch'],
     type: 'test',
   },
@@ -57,14 +58,21 @@ class Probe extends Component {
     time: 0,
   };
 
-  
   render() {
-    const props = helperMenu();
-
     return (
       <div>
         <h1>PROBE</h1>
-        <Testing tests={tests}/>
+        <MonacoEditor
+        width="800"
+        height="600"
+        language="javascript"
+        theme="vs-dark"
+        value="hellow world"
+        // options={options}
+        // onChange={::this.onChange}
+        // editorDidMount={::this.editorDidMount}
+      />
+        <Testing tests={tests} />
       </div>
     );
   }
